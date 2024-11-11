d8fc47d1d0cc156a7137eec40706f3e8f3508063
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-11-11 10:29:11 -0800

    Reduce default realism factor to .1d

--- Backend/Features/Spawner/Data/PrefabItem.cs
+++ Backend/Features/Spawner/Data/PrefabItem.cs
@@ -24,7 +24,7 @@ public class PrefabItem
     public double MaxSpeedKph { get; set; } = 20000;
     public double TargetDistance { get; set; } = 20000;
     public long FactionId { get; set; }
-    public double RealismFactor { get; set; } = 0.25d;
+    public double RealismFactor { get; set; } = 0.1d;
     public int MaxWeaponCount { get; set; } = 2;
 
     public PrefabEvents Events { get; set; } = new();
