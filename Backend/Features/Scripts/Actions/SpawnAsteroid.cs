73f2c6342160b31c0a36e83f24d9b208d78e4d70
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-12-16 22:33:11 -0800

    downcast value

--- Backend/Features/Scripts/Actions/SpawnAsteroid.cs
+++ Backend/Features/Scripts/Actions/SpawnAsteroid.cs
@@ -21,15 +21,14 @@ public class SpawnAsteroid(ScriptActionItem actionItem) : IScriptAction
 
     public async Task<ScriptActionResult> ExecuteAsync(ScriptContext context)
     {
-        var logger = context.ServiceProvider.CreateLogger<SpawnSectorAsteroid>();
         var random = context.ServiceProvider.GetRequiredService<IRandomProvider>().GetRandom();
         var pointGeneratorFactory = context.ServiceProvider.GetRequiredService<IPointGeneratorFactory>();
         var orleans = context.ServiceProvider.GetOrleans();
         var asteroidManagerGrain = orleans.GetAsteroidManagerGrain();
 
         var number = random.Next(1, 100);
-        var minTier = (int)actionItem.Properties.GetValueOrDefault("MinTier", 4);
-        var maxTier = (int)actionItem.Properties.GetValueOrDefault("MaxTier", 5);
+        var minTier = (int)(long)actionItem.Properties.GetValueOrDefault("MinTier", 4);
+        var maxTier = (int)(long)actionItem.Properties.GetValueOrDefault("MaxTier", 5);
 
         var tier = random.Next(minTier, maxTier);
 
