3beeae55f184e6737fb9d190b023a71e8bcd31eb
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-12-17 17:14:33 -0800

    Add Chance-bool script action

--- Backend/Features/Scripts/Actions/RandomScriptAction.cs
+++ Backend/Features/Scripts/Actions/RandomScriptAction.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Mod.DynamicEncounters.Common;
 using Mod.DynamicEncounters.Common.Interfaces;
 using Mod.DynamicEncounters.Features.Scripts.Actions.Data;
 using Mod.DynamicEncounters.Features.Scripts.Actions.Interfaces;
