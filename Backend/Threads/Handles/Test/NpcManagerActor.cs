816c3c52b76a5fc41d3ba3651585469144d48d71
Pedro Matos <voidrunner87@users.noreply.github.com>
2025-01-18 00:04:06 -0800

    Better logs

--- Backend/Threads/Handles/Test/NpcManagerActor.cs
+++ Backend/Threads/Handles/Test/NpcManagerActor.cs
@@ -9,7 +9,9 @@ using BotLib.Protocols;
 using BotLib.Protocols.Queuing;
 using Dapper;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Mod.DynamicEncounters.Database.Interfaces;
+using Mod.DynamicEncounters.Helpers;
 using Mod.DynamicEncounters.SDK;
 using NQ;
 
@@ -17,6 +19,7 @@ namespace Mod.DynamicEncounters.Threads.Handles.Test;
 
 public class NpcManagerActor(IServiceProvider provider) : Actor
 {
+    private readonly List<string> _users = [];
     private readonly List<Client> _clients = [];
 
     public override double FramesPerSecond { get; set; } = 1 / 4d;
@@ -29,15 +32,25 @@ public class NpcManagerActor(IServiceProvider provider) : Actor
         
         var result = (await db.QueryAsync("SELECT * FROM mod_npc_def")).ToList();
 
+        var logger = provider.CreateLogger<NpcManagerActor>();
+
         foreach (var item in result)
         {
-            var duClientFactory = provider.GetRequiredService<IDuClientFactory>();
-            var pi1 = LoginInformations.Impersonate(item.name,
-                item.name,
-                Environment.GetEnvironmentVariable("BOT_PASSWORD")!);
+            try
+            {
+                var duClientFactory = provider.GetRequiredService<IDuClientFactory>();
+                var pi1 = LoginInformations.Impersonate(item.name,
+                    item.name,
+                    Environment.GetEnvironmentVariable("BOT_PASSWORD")!);
 
-            var client = await Client.FromFactory(duClientFactory, pi1, allowExising: true);
-            _clients.Add(client);
+                var client = await Client.FromFactory(duClientFactory, pi1, allowExising: true);
+                _clients.Add(client);
+                _users.Add(item.name);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to auth to {User}", item.name as string);
+            }
         }
         
         await base.StartAsync(cancellationToken);
@@ -45,15 +58,27 @@ public class NpcManagerActor(IServiceProvider provider) : Actor
 
     public override async Task Tick(TimeSpan deltaTime, CancellationToken stoppingToken)
     {
+        var logger = provider.CreateLogger<NpcManagerActor>();
+        
+        var i = 0;
         foreach (var client in _clients)
         {
-            await client.ImplementationClient.PlayerUpdate(new PlayerUpdate
+            try
             {
-                playerId = client.PlayerId,
-                position = new Vec3(),
-                constructId = 0,
-                time = TimePoint.Now(),
-            }, stoppingToken);
+                await client.ImplementationClient.PlayerUpdate(new PlayerUpdate
+                {
+                    playerId = client.PlayerId,
+                    position = new Vec3(),
+                    constructId = 0,
+                    time = TimePoint.Now(),
+                }, stoppingToken);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to update pos of {User}", _users[i]);
+            }
+
+            i++;
         }
 
         await Task.Yield();
