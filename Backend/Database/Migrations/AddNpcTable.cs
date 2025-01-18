4d1a662ed2c9abfb4a9d92aaed596fbe35c2a583
Pedro Matos <voidrunner87@users.noreply.github.com>
2025-01-17 23:35:59 -0800

    Adds Npc Table

---
+++ Backend/Database/Migrations/AddNpcTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Mod.DynamicEncounters.Database.Migrations;
+
+[Migration(51)]
+public class AddNpcTable : Migration
+{
+    public const string NpcDefTable = "mod_npc_def";
+    private const string FactionTable = "mod_faction";
+    
+    public override void Up()
+    {
+        Create.Table(NpcDefTable)
+            .WithColumn("id").AsGuid().WithDefault(SystemMethods.NewGuid).PrimaryKey()
+            .WithColumn("name").AsString()
+            .WithColumn("faction_id").AsInt64().ForeignKey(FactionTable, "id");
+    }
+
+    public override void Down()
+    {
+        Delete.Table(NpcDefTable);
+    }
+}
