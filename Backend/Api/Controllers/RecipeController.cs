8cd45b6049aa06976d9645cc18698cbe05ea5f41
Pedro Matos <voidrunner87@users.noreply.github.com>
2025-01-24 23:38:10 -0800

    Recipe price calc

---
+++ Backend/Api/Controllers/RecipeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Mod.DynamicEncounters.Features.Market.Interfaces;
+
+namespace Mod.DynamicEncounters.Api.Controllers;
+
+[Route("recipe")]
+public class RecipeController : Controller
+{
+    [Route("{itemName}")]
+    [HttpGet]
+    public async Task<IActionResult> GetRecipePriceMap(string itemName)
+    {
+        var provider = ModBase.ServiceProvider;
+        var recipePriceCalculator = provider.GetRequiredService<IRecipePriceCalculator>();
+
+        var recipeMap = await recipePriceCalculator.GetItemPriceMap();
+
+        if (recipeMap.TryGetValue(itemName, out var result))
+        {
+            return Ok(result);
+        }
+        
+        return NotFound();
+    }
+}
