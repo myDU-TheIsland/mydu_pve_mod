f5ddfe5afef5372e5640e48aa64317f581a35774
Pedro Matos <voidrunner87@users.noreply.github.com>
2025-01-30 20:28:01 -0800

    Implemented escape logic

--- Backend/Features/Spawner/Behaviors/Skills/Services/RunAwaySkill.cs
+++ Backend/Features/Spawner/Behaviors/Skills/Services/RunAwaySkill.cs
@@ -2,7 +2,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Mod.DynamicEncounters.Common.Interfaces;
 using Mod.DynamicEncounters.Features.Common.Interfaces;
+using Mod.DynamicEncounters.Features.Scripts.Actions.Data;
+using Mod.DynamicEncounters.Features.Scripts.Actions.Extensions;
 using Mod.DynamicEncounters.Features.Spawner.Behaviors.Effects.Interfaces;
 using Mod.DynamicEncounters.Features.Spawner.Behaviors.Skills.Data;
 using Mod.DynamicEncounters.Features.Spawner.Data;
@@ -15,6 +18,9 @@ namespace Mod.DynamicEncounters.Features.Spawner.Behaviors.Skills.Services;
 
 public class RunAwaySkill(RunAwaySkill.RunAwaySkillItem skillItem) : BaseSkill(skillItem)
 {
+    public bool Triggered { get; set; }
+    public DateTime? LastContactDateTime { get; set; }
+    
     public override bool CanUse(BehaviorContext context)
     {
         return Active && context.IsAlive &&
@@ -25,18 +31,21 @@ public class RunAwaySkill(RunAwaySkill.RunAwaySkillItem skillItem) : BaseSkill(s
     {
         var provider = context.Provider;
         var areaScanService = provider.GetRequiredService<IAreaScanService>();
+        var dateTimeProvider = provider.GetRequiredService<IDateTimeProvider>();
         
         var targetConstructId = context.GetTargetConstructId();
         if (!context.Position.HasValue) return;
 
-        if (skillItem.TriggerOnPlayerContact)
+        var contacts = (await areaScanService.ScanForPlayerContacts(
+            context.ConstructId, 
+            context.Position.Value, 
+            skillItem.ScanRange)).ToList();
+        
+        if (!Triggered && skillItem.TriggerOnPlayerContact)
         {
-            var contacts = await areaScanService.ScanForPlayerContacts(
-                context.ConstructId, 
-                context.Position.Value, 
-                skillItem.ScanRange);
-            
-            if (!contacts.Any()) return;
+            if (contacts.Count == 0) return;
+
+            Triggered = true;
         }
         
         Vec3 direction;
@@ -51,6 +60,25 @@ public class RunAwaySkill(RunAwaySkill.RunAwaySkillItem skillItem) : BaseSkill(s
 
         context.SetOverrideTargetMovePosition(context.Position + direction * skillItem.MovePositionDistance);
         context.Effects.Activate<CooldownEffect>(TimeSpan.FromSeconds(skillItem.CooldownSeconds));
+
+        var now = dateTimeProvider.UtcNow();
+        
+        if (contacts.Count > 0)
+        {
+            LastContactDateTime = now;
+        }
+
+        if (!LastContactDateTime.HasValue) return;
+        
+        var deltaTime = now - LastContactDateTime.Value;
+        if (deltaTime > TimeSpan.FromMinutes(skillItem.EscapeMinTimeMinutes))
+        {
+            var scriptAction = provider.GetScriptAction(skillItem.OnEscapeScript);
+            var scriptContext = context.GetScriptContext();
+            await scriptAction.ExecuteAsync(scriptContext);
+
+            Active = false;
+        }
     }
 
     public static RunAwaySkill Create(JToken jObj)
@@ -64,6 +92,11 @@ public class RunAwaySkill(RunAwaySkill.RunAwaySkillItem skillItem) : BaseSkill(s
     {
         [JsonProperty] public double MovePositionDistance { get; set; } = DistanceHelpers.OneSuInMeters * 20;
         [JsonProperty] public bool TriggerOnPlayerContact { get; set; }
-        [JsonProperty] public float ScanRange { get; set; } = DistanceHelpers.OneSuInMeters * 4;
+        [JsonProperty] public double ScanRange { get; set; } = DistanceHelpers.OneSuInMeters * 4;
+        [JsonProperty] public double EscapeMinTimeMinutes { get; set; } = 1;
+        [JsonProperty] public ScriptActionItem OnEscapeScript { get; set; } = new()
+        {
+            Type = "delete"
+        };
     }
 }
