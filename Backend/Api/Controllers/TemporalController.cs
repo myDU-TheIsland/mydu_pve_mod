b199f1b9bff53b35c7bbc92ed9fcfca7d01ffdc7
Pedro Matos <voidrunner87@users.noreply.github.com>
2025-02-13 19:59:11 -0800

    Temporal Controller

---
+++ Backend/Api/Controllers/TemporalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mod.DynamicEncounters.Api.Controllers;
+
+[Route("temporal")]
+public class TemporalController : Controller
+{
+    [HttpGet]
+    [Route("config")]
+    public IActionResult GetConfig()
+    {
+        return Ok(new
+        {
+            Host = Environment.GetEnvironmentVariable("TEMPORAL_HOST")
+        });
+    }
+}
